91ee7c46ffa0bc22a421dfcade7f256d6e4fdff9
Soumadeep Nayak <soumadeepnayak@gmail.com>
2026-08-03 00:18:31 +0530

    Time: 8 ms (71.50%) | Memory: 12.4 MB (55.11%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        revertedNumber = 0
+        while x > revertedNumber:
+            revertedNumber = revertedNumber * 10 + x % 10
+            x //= 10
+
+        return x == revertedNumber or x == revertedNumber // 10
